7dd65d9e6c7a514d2f382ba0b963bf5105d144aa
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-09-16 00:06:11 +0530

    Code Refactoring

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function timer() {
-    var date1 = new Date();
-    var date2 = new Date("12/31/2019");
-    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    var timeDiffInSecond = (timeDiff / 1000);
-    
-    const days = Math.floor(timeDiffInSecond/3600/24);
-    const hours = Math.floor(timeDiffInSecond/3600)%24;
-    const minutes = Math.floor(timeDiffInSecond/60)%60;
-    const seconds = Math.floor(timeDiffInSecond)%60;
-    console.log(timeDiffInSecond, days, hours, minutes,seconds);
+  let date1 = new Date();
+  let date2 = new Date("12/31/2019");
+  let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  let timeDiffInSecond = timeDiff / 1000;
+
+  let days = Math.floor(timeDiffInSecond / 3600 / 24);
+  let hours = Math.floor(timeDiffInSecond / 3600) % 24;
+  let minutes = Math.floor(timeDiffInSecond / 60) % 60;
+  let seconds = Math.floor(timeDiffInSecond) % 60;
+
+  document.getElementById("current-days-element").innerHTML = days;
+  document.getElementById("current-hours-element").innerHTML = hours;
+  document.getElementById("current-minutes-element").innerHTML = minutes;
+  document.getElementById("current-seconds-element").innerHTML = seconds;
+
+  console.log(timeDiffInSecond, days, hours, minutes, seconds);
 }
 
 timer();
